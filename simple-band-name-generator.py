b155caf0c364a061dfc25595a7395aba26854ffa
ahmed essam <ahmedfawaz@hotmail.co.uk>
2025-03-21 08:05:53 +0200

    feat: add simple band name generator script

---
+++ simple-band-name-generator.py
@@ -0,0 +1,6 @@
+print("Welcome to the band name generator")
+city_name = input("what's name of your city?\n")
+
+pet_name = input("what's your pet name?\n")
+
+print("Your band name could be " + city_name + " " + pet_name)
